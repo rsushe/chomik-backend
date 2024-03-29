9f415610c7d9b5b50e83dbea1f34fab5c2f3bc27
rsushe <sushenkoroman2003@gmail.com>
2024-03-29 17:27:26 +0300

    CHOMIK-13 refactoring

--- chomik-gateway/src/main/kotlin/com/chomik/core/gateway/config/ApplicationConfiguration.kt
+++ chomik-gateway/src/main/kotlin/com/chomik/core/gateway/config/ApplicationConfiguration.kt
@@ -1,6 +1,6 @@
 package com.chomik.core.gateway.config
 
-import com.chomik.storage.client.service.AdvertService
+import com.chomik.storage.client.service.AdvertClient
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -10,5 +10,5 @@ import org.springframework.scheduling.annotation.EnableScheduling
 @EnableScheduling
 class ApplicationConfiguration {
     @Bean
-    fun advertService(@Value("\${chomik.storage.url}") storageUrl: String) = AdvertService(storageUrl)
+    fun advertService(@Value("\${chomik.storage.url}") storageUrl: String) = AdvertClient(storageUrl)
 }
