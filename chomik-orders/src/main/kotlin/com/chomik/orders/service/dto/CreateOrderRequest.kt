62027428b5e48a7bb7a60e9b5a8aa003e9876953
maksgirya <girya@lanit.ru>
2024-03-29 23:17:58 +0300

    CHOMIK-16 order add count field

--- chomik-orders/src/main/kotlin/com/chomik/orders/service/dto/CreateOrderRequest.kt
+++ chomik-orders/src/main/kotlin/com/chomik/orders/service/dto/CreateOrderRequest.kt
@@ -1,7 +1,11 @@
 package com.chomik.orders.service.dto
 
 
+import jakarta.persistence.Column
+import jakarta.validation.constraints.Max
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
 
 data class CreateOrderRequest (
 
@@ -10,5 +14,10 @@ data class CreateOrderRequest (
 
     @field:NotBlank(message = "Price must not be null")
     val advertId: String,
+
+    @field:NotNull(message = "sneakerCount must not be null")
+    @field:Max(value = 100, message = "sneakerCount must be less than 100")
+    @field:Min(value = 1, message = "sneakerCount must be more than 0")
+    val sneakerCount: Int,
 
 )
