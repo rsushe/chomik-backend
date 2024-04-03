214b0c2959605706e635e35024b531c38da45646
rsushe <sushenkoroman2003@gmail.com>
2024-04-03 17:34:41 +0300

    another try

--- chomik-gateway/src/test/kotlin/com/chomik/core/gateway/ChomikTestConfiguration.kt
+++ chomik-gateway/src/test/kotlin/com/chomik/core/gateway/ChomikTestConfiguration.kt
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.provisioning.InMemoryUserDetailsManager
 
 @Configuration
@@ -32,4 +34,7 @@ class ChomikTestConfiguration {
 
         return InMemoryUserDetailsManager(listOf(userDetails, sellerDetails))
     }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
 }
