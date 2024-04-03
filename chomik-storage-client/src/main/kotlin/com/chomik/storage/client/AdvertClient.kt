8647095a81a7c0185fac73641897f3bc67d59c77
rsushe <sushenkoroman2003@gmail.com>
2024-04-03 23:38:20 +0300

    CHOMIK-24 fix magic strings

--- chomik-storage-client/src/main/kotlin/com/chomik/storage/client/AdvertClient.kt
+++ chomik-storage-client/src/main/kotlin/com/chomik/storage/client/AdvertClient.kt
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 
+private const val ADVERT_URL_PREFIX = "api/v1/advert"
+
 class AdvertClient(
     private val baseUrl: String,
     private val restTemplate: RestTemplate = RestTemplate(),
 ) {
     fun getAllAdverts(): ResponseEntity<List<AdvertDto>> {
-        val uri = createBuilder("api/v1/advert")
+        val uri = createBuilder(ADVERT_URL_PREFIX)
 
         return restTemplate.exchange(
             uri.toUriString(),
@@ -24,25 +26,25 @@ class AdvertClient(
     }
 
     fun getAdvertById(id: String): ResponseEntity<AdvertDto> {
-        val uri = createBuilder("api/v1/advert/${id}")
+        val uri = createBuilder("${ADVERT_URL_PREFIX}/${id}")
 
         return restTemplate.getForEntity(uri.toUriString(), AdvertDto::class.java)
     }
 
     fun getAdvertsBySellerId(id: String): ResponseEntity<AdvertDto> {
-        val uri = createBuilder("api/v1/advert/seller/${id}")
+        val uri = createBuilder("${ADVERT_URL_PREFIX}/seller/${id}")
 
         return restTemplate.getForEntity(uri.toUriString(), AdvertDto::class.java)
     }
 
     fun createAdvert(request: SaveAdvertRequest): ResponseEntity<AdvertDto> {
-        val uri = createBuilder("api/v1/advert")
+        val uri = createBuilder(ADVERT_URL_PREFIX)
 
         return restTemplate.postForEntity(uri.toUriString(), request, AdvertDto::class.java)
     }
 
     fun updateAdvert(id: String, updateRequest: SaveAdvertRequest): ResponseEntity<AdvertDto> {
-        val uri = createBuilder("api/v1/advert/${id}")
+        val uri = createBuilder("${ADVERT_URL_PREFIX}/${id}")
 
         return restTemplate.postForEntity(uri.toUriString(), updateRequest, AdvertDto::class.java)
     }
--- chomik-storage-client/src/main/kotlin/com/chomik/storage/client/SneakersClient.kt
+++ chomik-storage-client/src/main/kotlin/com/chomik/storage/client/SneakersClient.kt
@@ -8,12 +8,14 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 
+private const val SNEAKERS_URL_PREFIX = "api/v1/sneakers"
+
 class SneakersClient(
     private val baseUrl: String,
     private val restTemplate: RestTemplate = RestTemplate(),
 ) {
     fun getAllSneakers(): ResponseEntity<List<SneakersDto>> {
-        val uri = createBuilder("api/v1/sneakers")
+        val uri = createBuilder(SNEAKERS_URL_PREFIX)
 
         return restTemplate.exchange(
             uri.toUriString(),
@@ -24,25 +26,25 @@ class SneakersClient(
     }
 
     fun getSneakersById(id: String): ResponseEntity<SneakersDto> {
-        val uri = createBuilder("api/v1/sneakers/${id}")
+        val uri = createBuilder("${SNEAKERS_URL_PREFIX}/${id}")
 
         return restTemplate.getForEntity(uri.toUriString(), SneakersDto::class.java)
     }
 
     fun createSneakers(request: SaveSneakersRequest): ResponseEntity<SneakersDto> {
-        val uri = createBuilder("api/v1/sneakers")
+        val uri = createBuilder(SNEAKERS_URL_PREFIX)
 
         return restTemplate.postForEntity(uri.toUriString(), request, SneakersDto::class.java)
     }
 
     fun updateSneakers(id: String, request: SaveSneakersRequest): ResponseEntity<SneakersDto> {
-        val uri = createBuilder("api/v1/sneakers/${id}")
+        val uri = createBuilder("${SNEAKERS_URL_PREFIX}/${id}")
 
         return restTemplate.postForEntity(uri.toUriString(), request, SneakersDto::class.java)
     }
 
     fun deleteSneakers(id: String): ResponseEntity<Void> {
-        val uri = createBuilder("api/v1/sneakers/${id}")
+        val uri = createBuilder("${SNEAKERS_URL_PREFIX}/${id}")
 
         return restTemplate.exchange(uri.toUriString(), HttpMethod.DELETE, null, Void::class.java)
     }
