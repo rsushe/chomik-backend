6f7e4562c7f75985ee8348c19e08e99338b575d1
maksgirya <girya@lanit.ru>
2024-04-05 00:19:25 +0300

    CHOMIK-39 delivery mock init v1.0

---
+++ delivery-mock/src/main/java/com/fakecdek/delivery/DeliveryMockApplication.java
@@ -0,0 +1,13 @@
+package com.fakecdek.delivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryMockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryMockApplication.class, args);
+	}
+
+}
---
+++ delivery-mock/src/test/java/com/fakecdek/delivery/DeliveryMockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fakecdek.delivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeliveryMockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
