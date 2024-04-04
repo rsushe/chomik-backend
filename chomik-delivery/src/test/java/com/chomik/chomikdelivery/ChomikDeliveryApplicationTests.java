d69076c27c1be7c02ce6b3c2fb744340ff485eb2
maksgirya <girya@lanit.ru>
2024-04-05 00:01:50 +0300

    CHOMIK-38 fix delivery service init v2.0

--- chomik-delivery/src/test/java/com/chomik/chomikdelivery/ChomikDeliveryApplicationTests.java
+++ chomik-delivery/src/test/java/com/chomik/chomikdelivery/ChomikDeliveryApplicationTests.java
@@ -2,8 +2,10 @@ package com.chomik.chomikdelivery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
-@SpringBootTest
+@SpringBootTest(classes = ChomikDeliveryApplication.class)
+@TestPropertySource("classpath:application-test.yml")
 class ChomikDeliveryApplicationTests {
 
 	@Test
