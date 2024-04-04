19634543addbca9a6bb6f8ea2d5b74f05569f1f4
maksgirya <girya@lanit.ru>
2024-04-04 23:53:54 +0300

    CHOMIK-38 delivery service init v1.0

---
+++ chomik-delivery/src/main/java/com/chomik/chomikdelivery/ChomikDeliveryApplication.java
@@ -0,0 +1,13 @@
+package com.chomik.chomikdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChomikDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChomikDeliveryApplication.class, args);
+    }
+
+}
---
+++ chomik-delivery/src/test/java/com/chomik/chomikdelivery/ChomikDeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chomik.chomikdelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChomikDeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
