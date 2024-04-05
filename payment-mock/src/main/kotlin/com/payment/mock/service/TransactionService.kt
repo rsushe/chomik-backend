5ee218acd0bd21c17a8eda1b3386c1c7ea8c97b2
rsushe <sushenkoroman2003@gmail.com>
2024-04-05 11:50:19 +0300

    CHOMIK-37 add endpoint for transaction processing

--- payment-mock/src/main/kotlin/com/payment/mock/service/TransactionService.kt
+++ payment-mock/src/main/kotlin/com/payment/mock/service/TransactionService.kt
@@ -5,6 +5,7 @@ import com.payment.mock.domain.Transaction
 import com.payment.mock.repository.TransactionRepository
 import jakarta.persistence.EntityManager
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 private const val SLEEP_TIME = 5000L
 
@@ -15,17 +16,15 @@ class TransactionService(
 ) {
     fun save(charge: Int) = transactionRepository.save(Transaction(charge = charge, status = PaymentStatus.CREATED))
 
+    @Transactional
     fun process(transactionId: String): Transaction {
-        var transaction = transactionRepository.findById(transactionId).orElseThrow {
+        val transaction = transactionRepository.findById(transactionId).orElseThrow {
             IllegalArgumentException("There is no transaction with id $transactionId")
         }
 
-        entityManager.clear()
-
         while (transaction.status == PaymentStatus.CREATED) {
             Thread.sleep(SLEEP_TIME)
-            transaction = transactionRepository.findById(transactionId).get()
-            entityManager.clear()
+            entityManager.refresh(transaction)
         }
 
         return transaction
