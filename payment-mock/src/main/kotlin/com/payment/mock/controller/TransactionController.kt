bc48ba30a11df20b317abedc2099ec7ea61a3bcc
rsushe <sushenkoroman2003@gmail.com>
2024-04-05 20:23:51 +0300

    CHOMIK-37 add endpoint for transaction processing

--- payment-mock/src/main/kotlin/com/payment/mock/controller/TransactionController.kt
+++ payment-mock/src/main/kotlin/com/payment/mock/controller/TransactionController.kt
@@ -3,6 +3,7 @@ package com.payment.mock.controller
 import com.payment.mock.controller.dto.ClientTransactionDto
 import com.payment.mock.domain.Transaction
 import com.payment.mock.service.TransactionService
+
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PathVariable
