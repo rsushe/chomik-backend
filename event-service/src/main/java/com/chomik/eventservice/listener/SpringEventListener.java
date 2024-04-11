62ad446f075ad5378b96ecfd0b21c4375c135118
maksgirya <girya@lanit.ru>
2024-04-12 00:38:23 +0300

    CHOMIK-52: refactoring

--- event-service/src/main/java/com/chomik/eventservice/listener/SpringEventListener.java
+++ event-service/src/main/java/com/chomik/eventservice/listener/SpringEventListener.java
@@ -2,7 +2,7 @@ package com.chomik.eventservice.listener;
 
 
 import com.chomik.eventservice.event.ChomikEvent;
-import com.chomik.eventservice.handler.DefaultLogEventHandler;
+import com.chomik.eventservice.exception.UnsupportedEventTypeException;
 import com.chomik.eventservice.handler.EventHandler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,69 +10,75 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-import static org.springframework.core.ResolvableType.forClass;
 
 @Component
 public class SpringEventListener {
 
     private static final ExecutorService executor = Executors.newFixedThreadPool(2);
-
     private static final Logger log = LoggerFactory.getLogger(SpringEventListener.class);
+    private final Map<Class<? extends ChomikEvent>, EventHandler<? extends ChomikEvent>> handlerMapByChomikEventClass;
 
-    private EventHandler<? extends ChomikEvent> defaultLogHandler;
-    private final Map<Class<? extends ChomikEvent>, EventHandler<? extends ChomikEvent>> handlerMapBychomikEventClass = new HashMap<>();
+    public SpringEventListener(@Autowired List<EventHandler<? extends ChomikEvent>> handlerList) {
+        handlerMapByChomikEventClass = new HashMap<>();
+        for (EventHandler<? extends ChomikEvent> handler : handlerList) {
 
-
-    public SpringEventListener(@Autowired final List<EventHandler<? extends ChomikEvent>> handlerList) {
-        for (final EventHandler<? extends ChomikEvent> handler : handlerList) {
-            if (handler instanceof DefaultLogEventHandler) {
-                defaultLogHandler = handler;
-                continue;
-            }
-            final Class<? extends ChomikEvent> chomikEventClass = getchomikEventGenericClass(handler);
-            if (handlerMapBychomikEventClass.containsKey(chomikEventClass)) {
+            Class<? extends ChomikEvent> chomikEventClass = getChomikEventGenericClass(handler);
+            if (handlerMapByChomikEventClass.containsKey(chomikEventClass)) {
                 throw new UnsupportedOperationException("Supported only 1 implementation handler on 1 ChomikEvent type class");
             }
-            this.handlerMapBychomikEventClass.put(chomikEventClass, handler);
-            log.info("Registered %s to handle events of type %s".formatted(handler.getClass(), chomikEventClass));
+            handlerMapByChomikEventClass.put(chomikEventClass, handler);
+            log.info("Registered {} to handle events of type {}", handler.getClass(), chomikEventClass);
         }
     }
 
-    private static Class<? extends ChomikEvent> getchomikEventGenericClass(final EventHandler<? extends ChomikEvent> handler) {
-        Class<?> generic = forClass(handler.getClass()).as(EventHandler.class).resolveGeneric(0);
-
+    private static Class<? extends ChomikEvent> getChomikEventGenericClass(EventHandler<? extends ChomikEvent> handler) {
+        Class<?> generic = Resolver.resolveGeneric(handler.getClass(), EventHandler.class, 0);
         if (generic == null) {
-            throw new UnsupportedOperationException(
-                    "Couldn't get generic class(extended ChomikEvent) from the handler class%s".formatted(handler.toString()));
+            throw new UnsupportedOperationException("Couldn't get generic class(extended ChomikEvent) from the handler class %s".formatted(handler.toString()));
         }
-
-        return (Class<? extends ChomikEvent>) forClass(handler.getClass()).as(EventHandler.class).resolveGeneric(0);
+        return (Class<? extends ChomikEvent>) generic;
     }
 
     @EventListener
-    public void handle(final ChomikEvent event) {
+    public void handle(ChomikEvent event) {
         executor.submit(() -> handleInternal(event));
     }
 
     protected <T extends ChomikEvent> void handleInternal(T event) {
-
         try {
             getHandler(event).handle(event);
-        } catch (final Exception e) {
+        } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
     }
 
-    <T extends ChomikEvent> EventHandler<T> getHandler(final T event) {
-        if (!handlerMapBychomikEventClass.containsKey(event.getClass())) {
-            return (EventHandler<T>) defaultLogHandler;
+    @SuppressWarnings("unchecked")
+    <T extends ChomikEvent> EventHandler<T> getHandler(T event) {
+        if (!handlerMapByChomikEventClass.containsKey(event.getClass())) {
+            throw new UnsupportedEventTypeException("Couldn't resolve handler for Event type: %s".formatted(event.getClass().toString()));
         }
-        return (EventHandler<T>) handlerMapBychomikEventClass.get(event.getClass());
+        return (EventHandler<T>) handlerMapByChomikEventClass.get(event.getClass());
+    }
+
+    private static class Resolver {
+        static Class<?> resolveGeneric(Class<?> clazz, Class<?> interfaceClass, int index) {
+            Type[] types = clazz.getGenericInterfaces();
+            for (Type type : types) {
+                if (type instanceof ParameterizedType && ((ParameterizedType) type).getRawType().equals(interfaceClass)) {
+                    return (Class<?>) ((ParameterizedType) type).getActualTypeArguments()[index];
+                }
+            }
+            return null;
+        }
     }
 }
+
+
