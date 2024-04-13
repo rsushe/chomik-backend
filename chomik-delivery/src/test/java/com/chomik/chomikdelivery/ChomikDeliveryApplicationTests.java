e3dbdff58adb7df1ea0e9ffd69fa7466cfdea061
maksgirya <girya@lanit.ru>
2024-04-13 18:12:27 +0300

    fix

--- chomik-delivery/src/test/java/com/chomik/chomikdelivery/ChomikDeliveryApplicationTests.java
+++ chomik-delivery/src/test/java/com/chomik/chomikdelivery/ChomikDeliveryApplicationTests.java
@@ -2,10 +2,12 @@ package com.chomik.chomikdelivery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest(classes = ChomikDeliveryApplication.class)
 @TestPropertySource("classpath:application-test.yml")
+@ActiveProfiles("test")
 class ChomikDeliveryApplicationTests {
 
 	@Test
