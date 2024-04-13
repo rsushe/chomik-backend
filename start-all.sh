9dd21b535d2dd4c79d4fe17860ab22d787dcba98
maksgirya <girya@lanit.ru>
2024-04-13 18:30:35 +0300

    fix

--- start-all.sh
+++ start-all.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 
-# List of modules
+# List of modules with artifactId and version
 modules=(
-  "chomik-gateway"
-  "chomik-orders"
-  "chomik-storage"
-  "chomik-payment"
-  "chomik-delivery"
-  "delivery-mock"
-  "payment-mock"
+  "chomik-gateway:0.0.1-SNAPSHOT"
+  "chomik-orders:0.0.1-SNAPSHOT"
+  "chomik-storage:0.0.1-SNAPSHOT"
+  "chomik-payment:0.0.1-SNAPSHOT"
+  "chomik-delivery:0.0.1-SNAPSHOT"
+  "delivery-mock:0.0.1-SNAPSHOT"
+  "payment-mock:0.0.1-SNAPSHOT"
 )
 
 # Loop through each module and run the corresponding JAR file
 for module in "${modules[@]}"
 do
-  jar_file="./${module}/target/${module}.jar"
+  artifact_id=$(echo "$module" | cut -d':' -f1)
+  version=$(echo "$module" | cut -d':' -f2)
+  jar_file="./${artifact_id}/target/${artifact_id}-${version}.jar"
   if [ -f "$jar_file" ]; then
+    echo "Running JAR file for module: $artifact_id"
     java -jar "$jar_file" &
   else
-    echo "JAR file not found for module: $module"
+    echo "JAR file not found for module: $artifact_id"
   fi
 done
