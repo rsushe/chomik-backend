cc423e815e948693c9a02a212950252f4c805ac7
maksgirya <girya@lanit.ru>
2024-04-13 18:07:31 +0300

    fix

--- chomik-storage/src/test/kotlin/com/chomik/storage/ChomikStorageApplicationTests.kt
+++ chomik-storage/src/test/kotlin/com/chomik/storage/ChomikStorageApplicationTests.kt
@@ -2,10 +2,12 @@ package com.chomik.storage
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest(classes = [ChomikStorageApplication::class])
 @TestPropertySource("classpath:application-test.yml")
+@ActiveProfiles("test")
 class ChomikStorageApplicationTests {
 
     @Test
