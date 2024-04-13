cdc6161e7a8a22c837102783ec274f62287e259c
maksgirya <girya@lanit.ru>
2024-04-13 18:17:40 +0300

    fix

--- payment-mock/src/test/kotlin/com/payment/mock/PaymentMockApplicationTests.kt
+++ payment-mock/src/test/kotlin/com/payment/mock/PaymentMockApplicationTests.kt
@@ -2,10 +2,12 @@ package com.payment.mock
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest(classes = [PaymentMockApplication::class])
 @TestPropertySource("classpath:application-test.yml")
+@ActiveProfiles("test")
 class PaymentMockApplicationTests {
 
 	@Test
