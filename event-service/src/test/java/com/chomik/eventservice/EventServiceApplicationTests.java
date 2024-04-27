1e66d8d9385a70afc7db1170e136f35bf5bf927c
Suschenko Roman <55081468+rsushe@users.noreply.github.com>
2024-04-27 15:05:15 +0300

    add run script (#59)

--- event-service/src/test/java/com/chomik/eventservice/EventServiceApplicationTests.java
+++ event-service/src/test/java/com/chomik/eventservice/EventServiceApplicationTests.java
@@ -1,10 +1,7 @@
 package com.chomik.eventservice;
 
-import com.winter.event.service.EventServiceApplication;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = EventServiceApplication.class)
 class EventServiceApplicationTests {
 
     @Test
--- run.sh
+++ run.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+export ENVIRONMENT=local
+
 mvn clean install
 
-mvn -f chomik-gateway/pom.xml spring-boot:run > /dev/null 2>&1 &
-mvn -f chomik-orders/pom.xml spring-boot:run > /dev/null 2>&1 &
-mvn -f chomik-storage/pom.xml spring-boot:run > /dev/null 2>&1 &
+nohup mvn -f chomik-gateway/pom.xml spring-boot:run &
+nohup mvn -f chomik-storage/pom.xml spring-boot:run &
+nohup mvn -f chomik-orders/pom.xml spring-boot:run &
+nohup mvn -f chomik-payment/pom.xml spring-boot:run &
+nohup mvn -f chomik-delivery/pom.xml spring-boot:run &
 
-
+nohup mvn -f delivery-mock/pom.xml spring-boot:run &
+nohup mvn -f payment-mock/pom.xml spring-boot:run &
