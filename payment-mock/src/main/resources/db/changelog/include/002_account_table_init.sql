13efde3721fd9d71d1b576952eca1f7292d677d4
rsushe <sushenkoroman2003@gmail.com>
2024-05-10 20:08:52 +0300

    improve yandex bank

--- payment-mock/src/main/resources/db/changelog/include/002_account_table_init.sql
+++ payment-mock/src/main/resources/db/changelog/include/002_account_table_init.sql
@@ -5,8 +5,8 @@ CREATE TABLE account
 (
     id          VARCHAR PRIMARY KEY,
     name        VARCHAR NOT NULL,
-    card_number VARCHAR NOT NULL,
-    cvv         INT     NOT NULL,
+    card_number VARCHAR UNIQUE NOT NULL,
+    cvv         INT     UNIQUE NOT NULL,
     expire_at   DATE    NOT NULL,
     balance     BIGINT  NOT NULL
 );
