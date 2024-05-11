58fe7099d2e99253811b61c95b222ddf9d81d2ce
rsushe <sushenkoroman2003@gmail.com>
2024-05-11 17:39:57 +0300

    change config

--- chomik-storage/src/main/kotlin/com/chomik/storage/config/SalesDatasourceConfig.kt
+++ chomik-storage/src/main/kotlin/com/chomik/storage/config/SalesDatasourceConfig.kt
@@ -5,33 +5,33 @@ import com.chomik.storage.repository.sales.SalesRepository
 import jakarta.persistence.EntityManagerFactory
 import liquibase.integration.spring.SpringLiquibase
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
 import java.util.*
-import javax.sql.DataSource
 
 @Configuration
-@EnableJpaRepositories(basePackageClasses = [SalesRepository::class], entityManagerFactoryRef = "salesEntityManager", transactionManagerRef = "transactionManager")
+@EnableJpaRepositories(
+    basePackageClasses = [SalesRepository::class],
+    entityManagerFactoryRef = "salesEntityManager",
+    transactionManagerRef = "transactionManager"
+)
 class SalesDatasourceConfig {
-    @Bean
-    @ConfigurationProperties(prefix = "datasource.sales.liquibase")
-    fun salesLiquibaseProperties(): LiquibaseProperties {
-        return LiquibaseProperties()
-    }
-
     @Bean
     fun salesLiquibase(
         @Value("\${datasource.sales.database}") dbName: String,
         @Value("\${datasource.sales.username}") username: String,
         @Value("\${datasource.sales.password}") pwd: String,
         @Value("\${datasource.sales.server}") server: String,
-        @Value("\${datasource.sales.port}") port: String): SpringLiquibase? {
-        return springLiquibase(salesDataSource(dbName, username, pwd, server, port), salesLiquibaseProperties())
+        @Value("\${datasource.sales.port}") port: String,
+        @Value("\${datasource.sales.liquibase.change-log}") liquibaseChangeLog: String
+    ): SpringLiquibase {
+        return SpringLiquibase().apply {
+            dataSource = salesDataSource(dbName, username, pwd, server, port)
+            changeLog = liquibaseChangeLog
+        }
     }
 
     @Bean(initMethod = "init", destroyMethod = "close")
@@ -40,27 +40,30 @@ class SalesDatasourceConfig {
         @Value("\${datasource.sales.username}") username: String,
         @Value("\${datasource.sales.password}") pwd: String,
         @Value("\${datasource.sales.server}") server: String,
-        @Value("\${datasource.sales.port}") port: String): AtomikosDataSourceBean {
+        @Value("\${datasource.sales.port}") port: String
+    ): AtomikosDataSourceBean {
         val dataSource = AtomikosDataSourceBean()
         dataSource.uniqueResourceName = "db2"
         dataSource.xaDataSourceClassName = "org.postgresql.xa.PGXADataSource"
         val xaProperties = Properties()
-        xaProperties.setProperty ( "user" , username );
-        xaProperties.setProperty ( "password" , pwd );
-        xaProperties.setProperty ( "serverName" , server );
-        xaProperties.setProperty ( "portNumber" , port );
-        xaProperties.setProperty ( "databaseName" , dbName );
+        xaProperties.setProperty("user", username)
+        xaProperties.setProperty("password", pwd)
+        xaProperties.setProperty("serverName", server)
+        xaProperties.setProperty("portNumber", port)
+        xaProperties.setProperty("databaseName", dbName)
         dataSource.xaProperties = xaProperties
         dataSource.setPoolSize(10)
         return dataSource
     }
 
     @Bean
-    fun salesEntityManager(@Value("\${datasource.sales.database}") dbName: String,
-                           @Value("\${datasource.sales.username}") username: String,
-                           @Value("\${datasource.sales.password}") pwd: String,
-                           @Value("\${datasource.sales.server}") server: String,
-                           @Value("\${datasource.sales.port}") port: String): EntityManagerFactory {
+    fun salesEntityManager(
+        @Value("\${datasource.sales.database}") dbName: String,
+        @Value("\${datasource.sales.username}") username: String,
+        @Value("\${datasource.sales.password}") pwd: String,
+        @Value("\${datasource.sales.server}") server: String,
+        @Value("\${datasource.sales.port}") port: String
+    ): EntityManagerFactory {
         val vendorAdapter = HibernateJpaVendorAdapter()
         val factory = LocalContainerEntityManagerFactoryBean()
         factory.jpaVendorAdapter = vendorAdapter
@@ -70,23 +73,11 @@ class SalesDatasourceConfig {
         jpaProperties["hibernate.dialect"] = "org.hibernate.dialect.DerbyDialect"
         jpaProperties["hibernate.current_session_context_class"] = "jta"
         jpaProperties["javax.persistence.transactionType"] = "jta"
-        jpaProperties["hibernate.transaction.manager_lookup_class"] = "com.atomikos.icatch.jta.hibernate3.TransactionManagerLookup"
+        jpaProperties["hibernate.transaction.manager_lookup_class"] =
+            "com.atomikos.icatch.jta.hibernate3.TransactionManagerLookup"
         jpaProperties["hibernate.hbm2ddl.auto"] = "create-drop"
         factory.setJpaProperties(jpaProperties)
         factory.afterPropertiesSet()
         return factory.getObject() as EntityManagerFactory
-    }
-
-    private fun springLiquibase(dataSource: DataSource, properties: LiquibaseProperties): SpringLiquibase? {
-        val liquibase = SpringLiquibase()
-        liquibase.dataSource = dataSource
-        liquibase.changeLog = properties.changeLog
-        liquibase.contexts = properties.contexts
-        liquibase.defaultSchema = properties.defaultSchema
-        liquibase.isDropFirst = properties.isDropFirst
-        liquibase.setShouldRun(properties.isEnabled)
-        liquibase.setChangeLogParameters(properties.parameters)
-        liquibase.setRollbackFile(properties.rollbackFile)
-        return liquibase
     }
 }
--- chomik-storage/src/main/kotlin/com/chomik/storage/config/StorageDatasourceConfig.kt
+++ chomik-storage/src/main/kotlin/com/chomik/storage/config/StorageDatasourceConfig.kt
@@ -6,33 +6,33 @@ import com.chomik.storage.repository.storage.SneakersRepository
 import jakarta.persistence.EntityManagerFactory
 import liquibase.integration.spring.SpringLiquibase
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseProperties
-import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter
 import java.util.*
-import javax.sql.DataSource
 
 @Configuration
-@EnableJpaRepositories(basePackageClasses = [AdvertRepository::class, SneakersRepository::class], entityManagerFactoryRef = "storageEntityManager", transactionManagerRef = "transactionManager")
+@EnableJpaRepositories(
+    basePackageClasses = [AdvertRepository::class, SneakersRepository::class],
+    entityManagerFactoryRef = "storageEntityManager",
+    transactionManagerRef = "transactionManager"
+)
 class StorageDatasourceConfig {
-    @Bean
-    @ConfigurationProperties(prefix = "datasource.storage.liquibase")
-    fun storageLiquibaseProperties(): LiquibaseProperties {
-        return LiquibaseProperties()
-    }
-
     @Bean
     fun storageLiquibase(
         @Value("\${datasource.storage.database}") dbName: String,
         @Value("\${datasource.storage.username}") username: String,
         @Value("\${datasource.storage.password}") pwd: String,
         @Value("\${datasource.storage.server}") server: String,
-        @Value("\${datasource.storage.port}") port: String): SpringLiquibase? {
-        return springLiquibase(storageDataSource(dbName, username, pwd, server, port), storageLiquibaseProperties())
+        @Value("\${datasource.storage.port}") port: String,
+        @Value("\${datasource.storage.liquibase.change-log}") liquibaseChangeLog: String
+    ): SpringLiquibase {
+        return SpringLiquibase().apply {
+            dataSource = storageDataSource(dbName, username, pwd, server, port)
+            changeLog = liquibaseChangeLog
+        }
     }
 
     @Bean(initMethod = "init", destroyMethod = "close")
@@ -41,27 +41,30 @@ class StorageDatasourceConfig {
         @Value("\${datasource.storage.username}") username: String,
         @Value("\${datasource.storage.password}") pwd: String,
         @Value("\${datasource.storage.server}") server: String,
-        @Value("\${datasource.storage.port}") port: String): AtomikosDataSourceBean {
+        @Value("\${datasource.storage.port}") port: String
+    ): AtomikosDataSourceBean {
         val dataSource = AtomikosDataSourceBean()
         dataSource.uniqueResourceName = "db1"
         dataSource.xaDataSourceClassName = "org.postgresql.xa.PGXADataSource"
         val xaProperties = Properties()
-        xaProperties.setProperty ( "user" , username );
-        xaProperties.setProperty ( "password" , pwd );
-        xaProperties.setProperty ( "serverName" , server );
-        xaProperties.setProperty ( "portNumber" , port );
-        xaProperties.setProperty ( "databaseName" , dbName );
+        xaProperties.setProperty("user", username)
+        xaProperties.setProperty("password", pwd)
+        xaProperties.setProperty("serverName", server)
+        xaProperties.setProperty("portNumber", port)
+        xaProperties.setProperty("databaseName", dbName)
         dataSource.xaProperties = xaProperties
         dataSource.setPoolSize(10)
         return dataSource
     }
 
     @Bean
-    fun storageEntityManager(@Value("\${datasource.storage.database}") dbName: String,
-                             @Value("\${datasource.storage.username}") username: String,
-                             @Value("\${datasource.storage.password}") pwd: String,
-                             @Value("\${datasource.storage.server}") server: String,
-                             @Value("\${datasource.storage.port}") port: String): EntityManagerFactory {
+    fun storageEntityManager(
+        @Value("\${datasource.storage.database}") dbName: String,
+        @Value("\${datasource.storage.username}") username: String,
+        @Value("\${datasource.storage.password}") pwd: String,
+        @Value("\${datasource.storage.server}") server: String,
+        @Value("\${datasource.storage.port}") port: String
+    ): EntityManagerFactory {
         val vendorAdapter = HibernateJpaVendorAdapter()
         val factory = LocalContainerEntityManagerFactoryBean()
         factory.jpaVendorAdapter = vendorAdapter
@@ -71,23 +74,11 @@ class StorageDatasourceConfig {
         jpaProperties["hibernate.dialect"] = "org.hibernate.dialect.DerbyDialect"
         jpaProperties["hibernate.current_session_context_class"] = "jta"
         jpaProperties["javax.persistence.transactionType"] = "jta"
-        jpaProperties["hibernate.transaction.manager_lookup_class"] = "com.atomikos.icatch.jta.hibernate3.TransactionManagerLookup"
+        jpaProperties["hibernate.transaction.manager_lookup_class"] =
+            "com.atomikos.icatch.jta.hibernate3.TransactionManagerLookup"
         jpaProperties["hibernate.hbm2ddl.auto"] = "create-drop"
         factory.setJpaProperties(jpaProperties)
         factory.afterPropertiesSet()
         return factory.getObject() as EntityManagerFactory
-    }
-
-    private fun springLiquibase(dataSource: DataSource, properties: LiquibaseProperties): SpringLiquibase? {
-        val liquibase = SpringLiquibase()
-        liquibase.dataSource = dataSource
-        liquibase.changeLog = properties.changeLog
-        liquibase.contexts = properties.contexts
-        liquibase.defaultSchema = properties.defaultSchema
-        liquibase.isDropFirst = properties.isDropFirst
-        liquibase.setShouldRun(properties.isEnabled)
-        liquibase.setChangeLogParameters(properties.parameters)
-        liquibase.setRollbackFile(properties.rollbackFile)
-        return liquibase
     }
 }
