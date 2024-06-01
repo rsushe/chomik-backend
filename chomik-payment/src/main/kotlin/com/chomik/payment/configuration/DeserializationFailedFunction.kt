045aa3c37cd806d9893edfec85ebfa34051ed2e5
Suschenko Roman <55081468+rsushe@users.noreply.github.com>
2024-06-01 18:24:56 +0300

    Add kafka deserialization handler (#93)

---
+++ chomik-payment/src/main/kotlin/com/chomik/payment/configuration/DeserializationFailedFunction.kt
@@ -0,0 +1,17 @@
+package com.chomik.payment.configuration
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import org.springframework.kafka.support.serializer.FailedDeserializationInfo
+import org.springframework.stereotype.Component
+import java.util.function.Function
+
+@Component
+class DeserializationFailedFunction: Function<FailedDeserializationInfo, Any> {
+    private val log: Logger = LoggerFactory.getLogger(javaClass)
+
+    override fun apply(t: FailedDeserializationInfo) {
+        log.error("Error while deserialization. Message: ${String(t.data)}. Topic: ${t.topic}")
+    }
+}
